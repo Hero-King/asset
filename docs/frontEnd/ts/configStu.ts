9adc3aca189f3c1df91d822e83bfd51cd3fe616a
wangjunjie <SV00249433@Svolt.cn>
2023-08-10 20:09:07 +0800

    ts config

---
+++ docs/frontEnd/ts/configStu.ts
@@ -0,0 +1,9 @@
+// import * as dayjs from 'dayjs'
+import _ from 'underscore'
+import { ThrottleSettings } from 'underscore'
+console.log(_.VERSION)
+let aa: _.ThrottleSettings
+
+// let day: dayjs.Dayjs = dayjs()
+// const year = day.format('YYYY')
+// console.log(year)
