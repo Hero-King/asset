2316d080c266bdc0741d8bdd4fc3621c85368c61
HeroKing-hack <838774057@qq.com>
2024-05-18 22:08:20 +0800

    update monkey version

--- docs/.vuepress/public/chromeExtensions/tampermonkey.js
+++ docs/.vuepress/public/chromeExtensions/tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HeroKing Tampermonkey Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.1.4
+// @version      0.1.5
 // @description  HeroKing some scripts
 // @author       HeroKing
 // @include        *
