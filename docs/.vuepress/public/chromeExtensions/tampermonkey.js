c6dbfd70aabb6b46f72c12811f8ec23e89d9f82e
HeroKing-hack <838774057@qq.com>
2024-05-18 22:06:16 +0800

    更新视频脚本

--- docs/.vuepress/public/chromeExtensions/tampermonkey.js
+++ docs/.vuepress/public/chromeExtensions/tampermonkey.js
@@ -102,8 +102,8 @@
   if (location.host === 'www.czpx.cn') {
     // 视频时长记录: 进入页面时调用coursePlayServlet.do, 结束后调用savelearnprogressservlet
     // 不能多视频, 多页签同时
-    try {
-      sleep(3000).then(() => {
+    sleep(3000)
+      .then(() => {
         console.log('RUnnnnnn')
         window.check = emptyFun
         removeWebLimit()
@@ -117,100 +117,125 @@
           link.target = '_self'
           link?.click()
           return
-        }
-        let video
-        window.hanadleTime = Number.MAX_SAFE_INTEGER
-        const autoPlay = async () => {
-          autoAnswer()
-          video = select('video')
-          let start = select('#mse > xg-start')
-          if (!video) {
-            start.click()
-            await sleep(1500)
+        } else if (location.pathname.includes('coursePlayServlet.do')) {
+          let video
+          let autoAnswerLoading = false
+          window.hanadleTime = Number.MAX_SAFE_INTEGER
+          const autoPlay = async () => {
+            autoAnswer()
             video = select('video')
-            !video && console.error('自动播放失败')
+            let start = select('#mse > xg-start')
             if (!video) {
               start.click()
+              await sleep(1500)
               video = select('video')
+              if (!video) {
+                console.error('自动播放失败')
+                start.click()
+                video = select('video')
+              }
+              hanadleTime = isNaN(Date.now() + video.duration * 1000) ? Number.MAX_SAFE_INTEGER : Date.now() + video.duration * 1000
+              video.muted = true
             }
-            hanadleTime = Date.now() + video.duration * 1000
-            video.muted = true
+
+            video.addEventListener('loadedmetadata', function () {
+              console.log('loadedmetadata')
+              hanadleTime = Date.now() + this.duration * 1000
+            })
+
+            video.addEventListener('ended', function () {
+              console.log('ended')
+              hanadleTime = Number.MAX_SAFE_INTEGER
+            })
+            let retry = 3
+            video.addEventListener('error', function () {
+              console.log('视频加载失败')
+              if (retry > 0) {
+                select('#mse .xgplayer-error .xgplayer-error-refresh')?.click()
+                retry--
+              }
+            })
           }
 
-          video.addEventListener('loadedmetadata', function () {
-            console.log('loadedmetadata')
-            hanadleTime = Date.now() + this.duration * 1000
-          })
-
-          video.addEventListener('ended', function () {
-            console.log('ended')
-            hanadleTime = Number.MAX_SAFE_INTEGER
-          })
-          video.addEventListener('error', function () {
-            select('#mse .xgplayer-error .xgplayer-error-refresh')?.click()
-          })
-        }
-
-        setInterval(intervalTask, 5000)
-        clickIKnow()
-        autoPlay()
-        window.alert = function (msg) {
-          console.info('===== alert =====', msg)
-          if (msg.includes('获取出错,请刷新页面重试，谢谢！')) {
-            goStudyListAndStu()
-          }
-        }
-        const goStudyListAndStu = async () => {
-          const onlineStuList = Array.from(d.querySelectorAll('a')).filter((i) => i.href.includes('learnCourseListServlet.do'))
-          onlineStuList && onlineStuList[0].click()
-        }
-
-        function intervalTask() {
-          autoAnswer()
+          setInterval(intervalTask, 5000)
           clickIKnow()
-          manaulEnd()
-        }
-
-        function autoAnswer() {
-          const layer = document.getElementById('window')
-          if (layer && layer.style.display !== 'none') {
-            const pList = layer.querySelectorAll('p')
-            const radioList = layer.querySelectorAll('input[type=radio]')
-            let answer = null
-            pList.forEach((i) => {
-              if (i.align === 'right') {
-                answer = i.innerText.split('：')[1].trim()
-              }
-            })
-            radioList.forEach((i) => {
-              if (i.value === answer) {
-                i.click()
-                d.querySelector('div.layui-layer-btn.layui-layer-btn- > a')?.click() && console.log('完成自动答题')
-              }
-            })
+          autoPlay()
+          window.alert = function (msg) {
+            console.info('===== alert =====', msg)
+            if (msg.includes('获取出错,请刷新页面重试，谢谢！')) {
+              goStudyListAndStu()
+            }
           }
-        }
 
-        function clickIKnow() {
-          const list = Array.from(d.querySelectorAll('.layui-layer-title')).filter((i) => i.innerHTML.includes('视频播放提示'))
-          if (list && list.length > 0) {
-            const btns = d.querySelectorAll('.layui-layer.layui-layer-dialog div.layui-layer-btn > a')
-            Array.from(btns)
-              .filter((i) => i.innerHTML.includes('知道了'))[0]
-              .click()
+          const goStudyListAndStu = async () => {
+            const onlineStuList = Array.from(d.querySelectorAll('a')).filter((i) => i.href.includes('learnCourseListServlet.do'))
+            onlineStuList && onlineStuList[0].click()
           }
-        }
 
-        function manaulEnd() {
-          if (Date.now() > hanadleTime + 1000 * 5) {
-            video.dispatchEvent(new Event('ended'))
-            console.log(video.src, '=====')
+          function intervalTask() {
+            autoAnswer()
+            clickIKnow()
+            manaulEnd()
+          }
+
+          /**
+           * 执行提交的话间隔1min
+           * @returns
+           */
+          function autoAnswer() {
+            const layer = document.getElementById('window')
+            if (layer && layer.style.display !== 'none') {
+              if (autoAnswerLoading) {
+                return
+              }
+              autoAnswerLoading = true
+              setTimeout(() => {
+                autoAnswerLoading = false
+              }, 1000 * 60)
+              const pList = layer.querySelectorAll('p')
+              const radioList = layer.querySelectorAll('input[type=radio]')
+              let answer = null
+              pList.forEach((i) => {
+                if (i.align === 'right') {
+                  answer = i.innerText.split('：')[1].trim()
+                }
+              })
+              radioList.forEach((i) => {
+                if (i.value === answer) {
+                  i.click()
+                  d.querySelector('div.layui-layer-btn.layui-layer-btn- > a')?.click() && console.log('完成自动答题')
+                }
+              })
+            }
+          }
+
+          function clickIKnow() {
+            const list = Array.from(d.querySelectorAll('.layui-layer-title')).filter((i) => i.innerHTML.includes('视频播放提示'))
+            if (list && list.length > 0) {
+              const btns = d.querySelectorAll('.layui-layer.layui-layer-dialog div.layui-layer-btn > a')
+              Array.from(btns)
+                .filter((i) => i.innerHTML.includes('知道了'))[0]
+                .click()
+            }
+          }
+
+          function manaulEnd() {
+            if (Date.now() > hanadleTime + 1000 * 5) {
+              video.dispatchEvent(new Event('ended'))
+              console.log(video.src, '=====')
+            }
           }
         }
       })
-    } catch (error) {
-      console.error(error)
-      //   location.reload();
-    }
+      .catch((error) => {
+        console.error(error)
+        error = error.message ?? error
+        if (error.includes('click')) {
+          $.post('http://www.pushplus.plus/send', { token: localStorage.getItem('pushToken'), title: '视频脚本错误, 请检查', content: error })
+          setTimeout(() => {
+            location.reload()
+          }, 1000 * 60)
+        }
+      })
   }
 })()
