02cbe924e9e3400a2e5344f596e7c5de96c236c8
Hero-King <Hero-King@users.noreply.github.com>
2025-09-18 00:44:01 +0000

    deploy: 87326242284d007f74f46ebff273f82cc6af944f

---
+++ assets/index.html-DmYvTchR.js
@@ -0,0 +1,21 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as n,b as p,f as o,o as g}from"./app-MIZpsOfp.js";const i="/img/gsyApp/%E9%A2%84%E8%A7%88.png",a="/img/gsyApp/%E6%88%91%E7%9A%84.png",r="/img/gsyApp/%E6%B6%88%E6%81%AF%E4%B8%AD%E5%BF%83.png",s={};function m(c,t){return g(),n("div",null,[t[0]||(t[0]=p('<h1 id="demo" tabindex="-1"><a class="header-anchor" href="#demo"><span>Demo</span></a></h1><h2 id="储能-app" tabindex="-1"><a class="header-anchor" href="#储能-app"><span>储能 APP</span></a></h2><h3 id="预览" tabindex="-1"><a class="header-anchor" href="#预览"><span>预览</span></a></h3><figure><img src="'+i+'" alt="预览|200*200" tabindex="0" loading="lazy"><figcaption>预览|200*200</figcaption></figure><h3 id="我的" tabindex="-1"><a class="header-anchor" href="#我的"><span>我的</span></a></h3><figure><img src="'+a+'" alt="我的 =200*0" tabindex="0" loading="lazy"><figcaption>我的 =200*0</figcaption></figure><h4 id="消息中心" tabindex="-1"><a class="header-anchor" href="#消息中心"><span>消息中心</span></a></h4><figure><img src="'+r+'" alt="消息中心|200*200" tabindex="0" loading="lazy"><figcaption>消息中心|200*200</figcaption></figure>',8)),o(` 
+### 实时
+
+![实时|200*200](/img/gsyApp/实时.png)
+
+#### 电池堆
+![电池堆|200*200](/img/gsyApp/电池堆.png)
+#### PCS
+![PCS|200*200](/img/gsyApp/PCS.png)
+#### 消防
+![消防|200*200](/img/gsyApp/消防.png)
+#### 通讯状态
+![通讯状态|200*200](/img/gsyApp/通讯状态.png)
+
+### 分析
+
+![分析|200*200](/img/gsyApp/分析.png)
+
+### 日志
+
+![日志|200*200](/img/gsyApp/日志.png) `)])}const l=e(s,[["render",m]]),y=JSON.parse('{"path":"/frontEnd/demo/","title":"Demo","lang":"zh-CN","frontmatter":{"description":"Demo 储能 APP 预览 预览|200*200预览|200*200 我的 我的 =200*0我的 =200*0 消息中心 消息中心|200*200消息中心|200*200","head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"Demo\\",\\"image\\":[\\"https://blog.heroking.top/img/gsyApp/预览.png\\",\\"https://blog.heroking.top/img/gsyApp/我的.png\\",\\"https://blog.heroking.top/img/gsyApp/消息中心.png\\",\\"https://blog.heroking.top/img/gsyApp/实时.png\\",\\"https://blog.heroking.top/img/gsyApp/电池堆.png\\",\\"https://blog.heroking.top/img/gsyApp/PCS.png\\",\\"https://blog.heroking.top/img/gsyApp/消防.png\\",\\"https://blog.heroking.top/img/gsyApp/通讯状态.png\\",\\"https://blog.heroking.top/img/gsyApp/分析.png\\",\\"https://blog.heroking.top/img/gsyApp/日志.png\\"],\\"dateModified\\":\\"2025-09-18T00:43:12.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"HeroKing\\",\\"url\\":\\"https://github.com/Hero-King\\",\\"email\\":\\"838774057@qq.com\\"}]}"],["meta",{"property":"og:url","content":"https://blog.heroking.top/frontEnd/demo/"}],["meta",{"property":"og:site_name","content":"HeroKing博客"}],["meta",{"property":"og:title","content":"Demo"}],["meta",{"property":"og:description","content":"Demo 储能 APP 预览 预览|200*200预览|200*200 我的 我的 =200*0我的 =200*0 消息中心 消息中心|200*200消息中心|200*200"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:image","content":"https://blog.heroking.top/img/gsyApp/预览.png"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2025-09-18T00:43:12.000Z"}],["meta",{"property":"article:modified_time","content":"2025-09-18T00:43:12.000Z"}]]},"git":{"createdTime":1758156192000,"updatedTime":1758156192000,"contributors":[{"name":"HeroKing-web","username":"HeroKing-web","email":"838774057@qq.com","commits":1,"url":"https://github.com/HeroKing-web"}]},"readingTime":{"minutes":0.38,"words":114},"filePathRelative":"frontEnd/demo/readme.md","excerpt":"\\n<h2>储能 APP</h2>\\n<h3>预览</h3>\\n<figure><img src=\\"/img/gsyApp/预览.png\\" alt=\\"预览|200*200\\" tabindex=\\"0\\" loading=\\"lazy\\"><figcaption>预览|200*200</figcaption></figure>\\n<h3>我的</h3>\\n<figure><img src=\\"/img/gsyApp/我的.png\\" alt=\\"我的 =200*0\\" tabindex=\\"0\\" loading=\\"lazy\\"><figcaption>我的 =200*0</figcaption></figure>\\n<h4>消息中心</h4>","autoDesc":true}');export{l as comp,y as data};
