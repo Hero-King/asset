196c7f20f244bddd1dff6d938fed6f8d7a303695
Hero-King <Hero-King@users.noreply.github.com>
2025-12-18 06:23:03 +0000

    deploy: 5290a3d852c2a9363bcbaaa1a611f08c4406625f

---
+++ assets/index.html--Y6p88b7.js
@@ -0,0 +1,24 @@
+import{_ as o}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as p,b as i,f as n,a as e,o as r}from"./app-CmQPROSs.js";const g="/img/gsyApp/%E9%A2%84%E8%A7%88.png",a="/img/gsyApp/%E6%88%91%E7%9A%84.png",s="/img/gsyApp/%E6%B6%88%E6%81%AF%E4%B8%AD%E5%BF%83.png",m={methods:{throwError(){throw new Error("手动抛出的错误")},captureError(){this.Sentry.captureException(new Error("使用SDK API手动捕获错误和其他事件"))}}};function h(c,t,d,l,y,f){return r(),p("div",null,[t[0]||(t[0]=i('<h1 id="demo" tabindex="-1"><a class="header-anchor" href="#demo"><span>Demo</span></a></h1><h2 id="储能-app" tabindex="-1"><a class="header-anchor" href="#储能-app"><span>储能 APP</span></a></h2><h3 id="预览" tabindex="-1"><a class="header-anchor" href="#预览"><span>预览</span></a></h3><figure><img src="'+g+'" alt="预览|200*200" tabindex="0" loading="lazy"><figcaption>预览|200*200</figcaption></figure><h3 id="我的" tabindex="-1"><a class="header-anchor" href="#我的"><span>我的</span></a></h3><figure><img src="'+a+'" alt="我的 =200*0" tabindex="0" loading="lazy"><figcaption>我的 =200*0</figcaption></figure><h4 id="消息中心" tabindex="-1"><a class="header-anchor" href="#消息中心"><span>消息中心</span></a></h4><figure><img src="'+s+'" alt="消息中心|200*200" tabindex="0" loading="lazy"><figcaption>消息中心|200*200</figcaption></figure>',8)),n(`
+### 实时
+
+![实时|200*200](/img/gsyApp/实时.png)
+
+#### 电池堆
+![电池堆|200*200](/img/gsyApp/电池堆.png)
+#### PCS
+![PCS|200*200](/img/gsyApp/PCS.png)
+#### 消防
+![消防|200*200](/img/gsyApp/消防.png)
+#### 通讯状态
+![通讯状态|200*200](/img/gsyApp/通讯状态.png)
+
+### 分析
+
+![分析|200*200](/img/gsyApp/分析.png)
+
+### 日志
+
+![日志|200*200](/img/gsyApp/日志.png) `),t[1]||(t[1]=e("h2",{id:"sentry-demo",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#sentry-demo"},[e("span",null,"Sentry Demo")])],-1)),n(` <div>
+    <button @click="throwError">throw new Error</button>
+    <button @click="captureError">使用SDK API手动捕获错误和其他事件</button>
+  </div> `)])}const b=o(m,[["render",h]]),E=JSON.parse('{"path":"/frontEnd/demo/","title":"Demo","lang":"zh-CN","frontmatter":{"description":"Demo 储能 APP 预览 预览|200*200预览|200*200 我的 我的 =200*0我的 =200*0 消息中心 消息中心|200*200消息中心|200*200 Sentry Demo","head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"Demo\\",\\"image\\":[\\"https://blog.heroking.top/img/gsyApp/预览.png\\",\\"https://blog.heroking.top/img/gsyApp/我的.png\\",\\"https://blog.heroking.top/img/gsyApp/消息中心.png\\",\\"https://blog.heroking.top/img/gsyApp/实时.png\\",\\"https://blog.heroking.top/img/gsyApp/电池堆.png\\",\\"https://blog.heroking.top/img/gsyApp/PCS.png\\",\\"https://blog.heroking.top/img/gsyApp/消防.png\\",\\"https://blog.heroking.top/img/gsyApp/通讯状态.png\\",\\"https://blog.heroking.top/img/gsyApp/分析.png\\",\\"https://blog.heroking.top/img/gsyApp/日志.png\\"],\\"dateModified\\":\\"2025-12-18T06:21:43.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"HeroKing\\",\\"url\\":\\"https://github.com/Hero-King\\",\\"email\\":\\"838774057@qq.com\\"}]}"],["meta",{"property":"og:url","content":"https://blog.heroking.top/frontEnd/demo/"}],["meta",{"property":"og:site_name","content":"HeroKing博客"}],["meta",{"property":"og:title","content":"Demo"}],["meta",{"property":"og:description","content":"Demo 储能 APP 预览 预览|200*200预览|200*200 我的 我的 =200*0我的 =200*0 消息中心 消息中心|200*200消息中心|200*200 Sentry Demo"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:image","content":"https://blog.heroking.top/img/gsyApp/预览.png"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2025-12-18T06:21:43.000Z"}],["meta",{"property":"article:modified_time","content":"2025-12-18T06:21:43.000Z"}]]},"git":{"createdTime":1766038903000,"updatedTime":1766038903000,"contributors":[{"name":"HeroKing-web","username":"HeroKing-web","email":"838774057@qq.com","commits":1,"url":"https://github.com/HeroKing-web"}]},"readingTime":{"minutes":0.6,"words":179},"filePathRelative":"frontEnd/demo/readme.md","excerpt":"\\n<h2>储能 APP</h2>\\n<h3>预览</h3>\\n<figure><img src=\\"/img/gsyApp/预览.png\\" alt=\\"预览|200*200\\" tabindex=\\"0\\" loading=\\"lazy\\"><figcaption>预览|200*200</figcaption></figure>\\n<h3>我的</h3>\\n<figure><img src=\\"/img/gsyApp/我的.png\\" alt=\\"我的 =200*0\\" tabindex=\\"0\\" loading=\\"lazy\\"><figcaption>我的 =200*0</figcaption></figure>\\n<h4>消息中心</h4>","autoDesc":true}');export{b as comp,E as data};
